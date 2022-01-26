33fc92a119d7f626ea2bff2948e0e5396705d3c1
iorlandoi <gtz.orlando@gmail.com>
2022-01-25 21:19:19 -0700

    .

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
 let computerSelection;
 let playerSelection;
 let computerScore = 0;
-let humanScore = 0;
+let userScore = 0;
+let round = 1;
 
-function playerChoose() {
-    human = prompt("Play rock, scissors or paper for a best of 5 against AI, Write your pick below").toLowerCase();
-    if (human == "rock") {
-        playerSelection = 0;
-    } else if (human == "scissors") {
-        playerSelection = 1;
-    } else if (human == "paper") {
-        playerSelection =2;
-    } else {
-        alert("You misspelled or you didn't choose any!");
-        playerChoose();
-    }
-}   
+const btn1 = document.querySelector('#btn1');
+btn1.addEventListener('click', () => {
+    playerSelection = 0;
+    playRound();
+});
+
+const btn2 = document.querySelector('#btn2');
+btn2.addEventListener('click', () => {
+    playerSelection = 1;
+    playRound();
+});
+
+const btn3 = document.querySelector('#btn3');
+btn3.addEventListener('click', () => {
+    playerSelection = 2;
+    playRound();
+});
 
 function playRound() {
-    playerChoose()
-    
+    let user;
+
     computerSelection = Math.floor(Math.random()*3);
     if (computerSelection == 0) {
         computer = "rock";
@@ -28,29 +33,31 @@ function playRound() {
     } else {
         computer = "paper";
     }
+
+    computerSelection = Math.floor(Math.random()*3);
+    if (playerSelection == 0) {
+        user = "rock";
+    } else if (playerSelection == 1) {
+        user = "scissors";
+    } else {
+        user = "paper";
+    }
     
     if (playerSelection == computerSelection) {
-        alert("DRAW! Computer: " + computer + "; you: " + human);
+        round++;
+        document.getElementById("roundNum").innerHTML = round;
+        document.getElementById("result").innerHTML = "DRAW!";
     } else if ((playerSelection - computerSelection) == 1 || (playerSelection - computerSelection) == -2) {
-        alert("YOU LOSE! Computer: " + computer + "; you: " + human);
         computerScore++;
+        round++;
+        document.getElementById("roundNum").innerHTML = round;
+        document.getElementById("computerScore").innerHTML = computerScore;
+        document.getElementById("result").innerHTML = "YOU WIN!";
     } else {
-        alert("YOU WIN! Computer: " + computer + "; you: " + human);
-        humanScore++;
-    }
-
-    alert("Computer: " + computerScore + "; human: " + humanScore);
-}
-
-function game() {
-
-    while (computerScore < 3) {
-        playRound();
-        
-        if (humanScore == 3) {
-            break;
-        }
+        userScore++;
+        round++;
+        document.getElementById("roundNum").innerHTML = round;
+        document.getElementById("userScore").innerHTML = userScore;
+        document.getElementById("result").innerHTML = "YOU LOSE!";
     }
 }
-
-game();
